e1585fcbb1880d5be393b7c8858a0c9a181602c2
nnnagaraj <103841330+nnnagaraj@users.noreply.github.com>
2022-05-18 11:59:56 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,19 @@
+package week3.day1.assignment3;
+
+public class College {
+	
+	public static void collegeName() {
+		System.out.println("College Name:GKMCET ");
+		
+	}	
+	
+	public static void collegeCode() {
+		System.out.println("College Code:504");
+	
+	}
+	
+	public static void collegeRank() {
+		System.out.println("College Rank:23 ");
+	}
+	
+}
