a0314666fb2d00345e95737f293b78548a89300a
nnnagaraj <103841330+nnnagaraj@users.noreply.github.com>
2022-05-18 11:59:38 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,10 @@
+package week3.day1.assignment2;
+
+import week3.day1.assignment3.College;
+
+public class Department extends College {
+	public static void deptName() {
+		System.out.println("Department Name: Mechanical");
+	}
+
+}
